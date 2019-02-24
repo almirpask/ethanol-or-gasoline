d59006660a5a1f6e96aaba429f86e93ed0c76fae
almirpask <almirpask@hotmail.com>
2019-02-24 00:51:50 -0300

    finish

--- ethanol or gasoline/ViewController.swift
+++ ethanol or gasoline/ViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gasolinePrice: UITextField!
+    @IBOutlet weak var resultLegend: UILabel!
+    @IBOutlet weak var ethanolPrice: UITextField!
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        if let priceEthanol = ethanolPrice.text {
+            if let priceGasoline = gasolinePrice.text {
+                let validation = self.validateFields( priceEthanol: priceEthanol, priceGasoline: priceGasoline)
+                if validation {
+                    self.chooseBetterPrice( priceEthanol: priceEthanol, priceGasoline: priceGasoline)
+                } else {
+                    resultLegend.text = "Digite os preços para calcular!"
+                }
+            }
+        }
+    }
+    
+    func chooseBetterPrice(priceEthanol: String, priceGasoline: String) -> Void{
+        if let ethanol = Double(priceEthanol) {
+            if let gasoline = Double(priceGasoline){
+                /* (ethanol/ gasoline)
+                    if result => 0.7 gasoline is the better choose
+                    else ethanol is the better cooshe
+                 */
+                let result  = ethanol / gasoline
+                if(result >= 0.7){
+                    resultLegend.text = "Melhor ultilizar gasolina!"
+                }else {
+                    resultLegend.text = "Melhor ultilizar alcool!"
+                }
+            }
+        }
+    }
+    func validateFields(priceEthanol: String, priceGasoline: String) -> Bool{
+        var fieldsValidateds = true
+        if priceEthanol.isEmpty && priceGasoline.isEmpty{
+            fieldsValidateds = false
+        }
+        return fieldsValidateds
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
